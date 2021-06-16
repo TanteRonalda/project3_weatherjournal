b6255cde1e5815c51e1acfb502087b4a1f198f07
TanteRonalda <amikind08@gmail.com>
2021-06-16 20:25:45 +0200

    temp, date and user response are logged to the console

--- website/app.js
+++ website/app.js
@@ -1,21 +1,36 @@
-const { response } = require("express");
-
-/* Global Variables */
-let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zipcode=';
-const apiKey = '&appid=7d34d64a305fd6f7ece9ef8d4d1498c0';
+// Global Variables
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?';
+const apiKey = '7d34d64a305fd6f7ece9ef8d4d1498c0';
 
 // Event listener to add function to existing HTML DOM element
 document.getElementById('generate').addEventListener('click', doSomething);
 
-/* Function called by event listener */
+// Function called by event listener 
 function doSomething(e) {
-    const zipCode = document.getElementById('zip').value;
-    getWeather(baseURL, zipCode, apiKey)
+    const zip = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+    // given by Udacity, is this ready to use?
+    let d = new Date();
+    let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+    getWeather(baseURL, zip, apiKey)
+    .then(function(data) {
+      const temp = data.main.temp;
+      const someWeather = {
+        temp: temp,
+        date: newDate,
+        userResponse: feelings
+      };
+      console.log(someWeather);
+      // calling PostData function to post weather data to POST route on server
+      postData('/postroute', someWeather)
+    })
+    //another .then to chain function that changes UI
 };
 
-/* Function to GET Web API Data*/
-async function getWeather(baseURL, zipcode, apiKey) {
-    const res = await fetch(baseURL+zipcode+apiKey)
+//Function to GET Web API Data
+async function getWeather(baseURL, zip, apiKey) {
+    const res = await fetch(baseURL+"zip="+zip+"&appid="+apiKey)
     try {
         const weather = await res.json();
         console.log(weather)
@@ -26,28 +41,24 @@ async function getWeather(baseURL, zipcode, apiKey) {
       }
 };
 
-
 // Function to POST data 
 async function postData(url = '', weather = {}) {
   const res = await fetch(url, {
     method: POST,
     credentials: 'same-origin',
     headers: {
-      'Content-type': 'application/json',
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(weather),
   })
   try {
-    const responseData = await response.json();
-      return responseData
+    const newInput = await res.json();
+      return newInput;
   }catch(error){
   console.log('That is not what was supposed to happen', error);
   }
 }
 
-//Function to GET Project Data
+//Function to GET Project Data to change UI
 
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();*/
